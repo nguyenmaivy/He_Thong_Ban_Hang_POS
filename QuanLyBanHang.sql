698c1881db8c1227816d2c28ec6e666af379b910
tuanvudct1221 <43ntvu12a3ltk2122@gmail.com>
2025-04-10 22:33:38 +0700

    cap nhat ui login, register, ket noi csdl

---
+++ QuanLyBanHang.sql
@@ -0,0 +1,11 @@
+﻿USE He_Thong_Ban_Hang_POS;
+GO
+CREATE TABLE users (
+    id INT PRIMARY KEY IDENTITY(1,1),  
+    email NVARCHAR(255) NOT NULL UNIQUE, 
+    password NVARCHAR(255) NOT NULL,     
+    created_at DATETIME DEFAULT GETDATE() 
+);
+GO
+
+
